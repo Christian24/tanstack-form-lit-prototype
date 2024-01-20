11971726bf3a3ca779844fbf08c0dfe6a25ad3c3
Christian Siebmanns <christian.siebmanns@hotmail.de>
2024-01-20 17:20:45 +0100

    Removed unneeded dependencies

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-export default defineConfig({
-  test: {
-    browser: {
-      enabled: true,
-      name: "edge", // browser name is required
-    },
-  },
-});
--- web-test-runner.config.js
+++
@@ -1,6 +0,0 @@
-import { esbuildPlugin } from "@web/dev-server-esbuild";
-
-export default {
-  files: ["tests/**/*.test.ts", "tests/**/*.spec.ts"],
-  plugins: [esbuildPlugin({ ts: true })],
-};
